e60e44ff00013355ba5346d9048375534f244257
jeffersondlmatta <jeffersondlmatta@gmail.com>
2025-03-27 20:09:32 -0300

    front do projeto api com spring

---
+++ src/app/principal/principal.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-principal',
+  standalone: true,
+  imports: [],
+  templateUrl: './principal.component.html',
+  styleUrl: './principal.component.css'
+})
+export class PrincipalComponent {
+
+  //variavel - visibilidade dos btn
+  btnCadastro:boolean = true;
+
+
+
+}
